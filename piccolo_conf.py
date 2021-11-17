1d11da3f6bcb3d067ee156ed5637510e34d12542
Samuel Tatasurya <tatasurya.samuel@gmail.com>
2021-11-17 09:50:57 -0800

    Reverted last changes

--- piccolo_conf.py
+++ piccolo_conf.py
@@ -5,11 +5,7 @@ from piccolo.engine.postgres import PostgresEngine
 
 from settings import DB_URL
 
-pg_conf = {"dsn": DB_URL}
-if os.environ.get("GOOGLE_CLOUD_PROJECT", None):
-    pg_conf["ssl"] = "disable"  # using cloud_sql_proxy
-
-DB = PostgresEngine(config=pg_conf)
+DB = PostgresEngine(config={"dsn": DB_URL})
 
 # A list of paths to piccolo apps
 APP_REGISTRY = AppRegistry(apps=[
