c581ff64d6252d73fbfcf2ea5442e89f2fd4e390
Samuel Tatasurya <tatasurya.samuel@gmail.com>
2021-11-17 01:58:27 -0800

    Disabled SSL when using Cloud SQL proxy

--- piccolo_conf.py
+++ piccolo_conf.py
@@ -1,9 +1,15 @@
+import os
+
 from piccolo.conf.apps import AppRegistry
 from piccolo.engine.postgres import PostgresEngine
 
 from settings import DB_URL
 
-DB = PostgresEngine(config={"dsn": DB_URL})
+pg_conf = {"dsn": DB_URL}
+if os.environ.get("GOOGLE_CLOUD_PROJECT", None):
+    pg_conf["ssl"] = "disable"  # using cloud_sql_proxy
+
+DB = PostgresEngine(config=pg_conf)
 
 # A list of paths to piccolo apps
 APP_REGISTRY = AppRegistry(apps=[
